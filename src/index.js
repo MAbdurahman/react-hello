50eb69743b330f4a2bbde1c171fe0f1021bab5c8
MAbdurahman <mdbdrrhm@gmail.com>
2020-10-18 05:53:20 -0500

    Modify title and index.js to reflect Hello World

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloWorld = () => {
+  return(
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <HelloWorld/>, document.getElementById('root')
+);
+
+
 
 
 
